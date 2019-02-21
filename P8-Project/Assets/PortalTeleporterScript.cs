c5c85d463746de1d9fa5d1f7b97687a1cdcc2153
Guflix - Thomas Boelt <thomas@boelthansen.dk>
2019-02-21 13:29:09 +0100

    Playing with Mazes
    This aint working lol

--- P8-Project/Assets/PortalTeleporterScript.cs
+++ P8-Project/Assets/PortalTeleporterScript.cs
@@ -5,33 +5,33 @@ using UnityEngine;
 public class PortalTeleporterScript : MonoBehaviour {
 
     public Transform player;
-    public Transform receiver;
+    public Transform reciever;
 
     private bool playerIsOverlapping = false;
 
-	// Update is called once per frame
-	void Update () {
-
+    // Update is called once per frame
+    void Update()
+    {
         if (playerIsOverlapping)
         {
             Vector3 portalToPlayer = player.position - transform.position;
-            float dotproduct = Vector3.Dot(transform.right, portalToPlayer);
+            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
 
-            //If this is true - The player has moved across the portal 
-            if(dotproduct < 0f)
+            // If this is true: The player has moved across the portal
+            if (dotProduct < 0f)
             {
-                //Teleport player
-                float rotationDifference = -Quaternion.Angle(transform.rotation, receiver.rotation);
-                rotationDifference += 180;
-                player.Rotate(Vector3.up, rotationDifference);
+                // Teleport him!
+                float rotationDiff = -Quaternion.Angle(transform.rotation, reciever.rotation);
+                rotationDiff += 180;
+                player.Rotate(Vector3.up, rotationDiff);
 
-                Vector3 positionOffset = Quaternion.Euler(0f, rotationDifference, 0f) * portalToPlayer;
-                player.position = receiver.position + positionOffset;
+                Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
+                player.position = reciever.position + positionOffset;
 
                 playerIsOverlapping = false;
             }
         }
-	}
+    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -41,12 +41,11 @@ public class PortalTeleporterScript : MonoBehaviour {
         }
     }
 
-    void OnTriggerExit (Collider other)
+    void OnTriggerExit(Collider other)
     {
         if (other.tag == "Player")
         {
             playerIsOverlapping = false;
-   
         }
     }
- }
+}
