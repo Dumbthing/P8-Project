8f7d6dde4d55d8db09ed2ea92a01179d040f4e3a
Guflix - Thomas Boelt <thomas@boelthansen.dk>
2019-02-26 09:14:07 +0100

    monkaS

---
+++ P8-Project/Assets/Scripts/ProceduralPrefabGeneration.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralPrefabGeneration : MonoBehaviour {
+    
+    public GameObject[] roomArray;
+    public float rooms;
+    public float roomDistance;
+
+    void Start ()
+    {
+        for (int i = 0; i < rooms; i++)
+        {
+            int rand = Random.Range(0, 1);
+            Instantiate(roomArray[rand], new Vector3(roomDistance * i, 0, 0), Quaternion.identity);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+}
