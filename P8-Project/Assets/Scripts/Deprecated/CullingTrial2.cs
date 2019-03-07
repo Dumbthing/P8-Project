b18b70e6809b61400aafe369537f976e3436efcf
Jannik15 <jneerd15@student.aau.dk>
2019-03-07 15:43:46 +0100

    Significant culling mask implementation progress

--- P8-Project/Assets/Scripts/Deprecated/CullingTrial2.cs
+++ P8-Project/Assets/Scripts/Deprecated/CullingTrial2.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class CullingTrial2 : MonoBehaviour {
 
     Camera cam;
+    public GameObjectSearcher objSearch;
     LayerMask oldMask;
-    LayerMask newMask;
-    public GameObject[] ZTopMidRooms;
-    public GameObject startRoom;
+    public GameObject[] roomArr;
 
-    int currentRoom = 0;
-    int previousRoom;
+    public int portalEntered = 0;
+    public int lastPortalId;
+    public Vector3 lastPortalPos;
+    public int currentRoom = 0;
+    public int previousRoom = 0;
     public int currentLayer = 8;
     private int newLayer;
 
@@ -22,41 +24,110 @@ public class CullingTrial2 : MonoBehaviour {
         oldMask = cam.cullingMask; // Original culling mask, but with invisible layers.
     }
 
-    private void OnTriggerEnter(Collider entry)
-    {
-        if (entry.tag == "PortalToNext") // Portal that activates the next room (and sets it layer to visible)
-        {
-            // SetActiveness
-            ZTopMidRooms[currentRoom + 1].SetActive(true);
-            // Set layer visibiltiy
-            ShowLayer(ZTopMidRooms[currentRoom + 1].layer, entry);
-            // Increment
-            previousRoom = currentRoom;
-            entry.tag = "PortalToNextPassed";
-        }
-        else if (entry.tag == "PortalToNextPassed") // Portal that disables last created room
-        {
-            // SetActiveness
-            ZTopMidRooms[currentRoom + 1].SetActive(false);
-            // Set layer visibiltiy
-            HideLayer(ZTopMidRooms[currentRoom + 1].layer, entry);
-            // Increment
-            entry.tag = "PortalToNext";
-        }
-        else if (entry.tag == "PortalToPrevious") // Portal that disables previous room
-        {
-            ZTopMidRooms[currentRoom].SetActive(false);
-            HideLayer(ZTopMidRooms[currentRoom].layer, entry);
+    //private void OnTriggerExit(Collider col)
+    //{
+    //    //// Might be better to go into variable based system, rather than tag based.
 
-            currentRoom++;
-        }
-        else if (entry.tag == "PortalToPreviousPassed") // Portal that activates the previous room
+    //    if (col.tag == "PortalToNext") // Portal that activates the next room (and sets it layer to visible)
+    //    {
+    //        roomArr[currentRoom + 1].SetActive(true);
+    //        ShowLayer(roomArr[currentRoom + 1].layer, col);
+    //        previousRoom = currentRoom;
+    //        col.tag = "PortalToNextPassed";
+    //        ///// IF PortalToPreviousPassed 
+    //        /// Set it to PortalToPrevious
+            
+    //    }
+    //    else if (col.tag == "PortalToNextPassed") // Portal that disables last created room
+    //    {
+    //        roomArr[currentRoom + 1].SetActive(false);
+    //        HideLayer(roomArr[currentRoom + 1].layer, col);
+    //        col.tag = "PortalToNext";
+    //    }
+    //    else if (col.tag == "PortalToPrevious") // Portal that disables previous room
+    //    {
+    //        roomArr[currentRoom].SetActive(false);
+    //        HideLayer(roomArr[currentRoom].layer, col);
+    //        currentRoom++;
+    //        objSearch.SetActiveChild(roomArr[currentRoom].transform, "PortalToPrevious", true);
+    //        objSearch.SetActiveChild(roomArr[currentRoom].transform, "PortalToPreviousPassed", true);
+    //        objSearch.SetActiveChild(roomArr[currentRoom].transform, "PortalToNext", true);
+    //        objSearch.SetActiveChild(roomArr[currentRoom].transform, "PortalToNextPassed", true);
+    //        col.tag = "PortalToPreviousPassed";
+    //    }
+    //    else if (col.tag == "PortalToPreviousPassed") // Portal that activates the previous room
+    //    {
+    //        if (currentRoom != 0)
+    //        {
+    //            currentRoom--;
+    //            roomArr[currentRoom].SetActive(true);
+    //            ShowLayer(roomArr[currentRoom].layer, col);
+    //            col.tag = "PortalToPrevious";
+    //        }
+    //        else // Player went the wrong way :(
+    //        {
+
+    //        }
+    //    }
+    //}
+    
+    
+    private void OnTriggerExit(Collider col)
+    {
+        if (roomArr.Length > previousRoom+1)
         {
-            currentRoom--;
-            ZTopMidRooms[currentRoom].SetActive(true);
+            if (portalEntered == 0) // Portal that activates the next room (and sets it layer to visible)
+            {
+                if (col.transform.localPosition == lastPortalPos) // Should be same position as last portal id
+                {
+                    if (currentRoom == 0)
+                    {
+
+                    }
+                    else
+                    {
+                        Debug.Log("Same portal, before pass");
+                        currentRoom--;
+                        roomArr[currentRoom].SetActive(true);
+                        ShowLayer(roomArr[currentRoom].layer, col);
+                    }
+                }
+                else
+                {
+                    Debug.Log("col pos: " + col.transform.localPosition + ", last Portal pos: " + lastPortalPos);
+                    roomArr[currentRoom + 1].SetActive(true);
+                    ShowLayer(roomArr[currentRoom + 1].layer, col);
+                    previousRoom = currentRoom;
+                    portalEntered++;
+                    currentRoom++;
+                }
+            }
+            else if (portalEntered == 1) // Portal that disables last created room
+            {
+                if (col.GetInstanceID() == lastPortalId)
+                {
+                    Debug.Log("Same portal, after pass");
+                    roomArr[currentRoom].SetActive(false);
+                    HideLayer(roomArr[currentRoom].layer, col);
+                    portalEntered--;
+                    currentRoom--;
+                }
+                else
+                {
+                    roomArr[currentRoom - 1].SetActive(false);
+                    HideLayer(roomArr[currentRoom - 1].layer, col);
+                    objSearch.SetActiveChild(roomArr[currentRoom].transform, "PortalToPrevious", true);
+                    objSearch.SetActiveChild(roomArr[currentRoom].transform, "PortalToPreviousPassed", true);
+                    objSearch.SetActiveChild(roomArr[currentRoom].transform, "PortalToNext", true);
+                    objSearch.SetActiveChild(roomArr[currentRoom].transform, "PortalToNextPassed", true);
+                    previousRoom = currentRoom;
+                    portalEntered = 0;
+                }
+            }
+            lastPortalId = col.GetInstanceID();
+            lastPortalPos = col.transform.localPosition;
         }
     }
-    
 
     void GoThroughPortal(Collider portal)
     {
@@ -66,16 +137,17 @@ public class CullingTrial2 : MonoBehaviour {
     private void HideLayer(int layerToHide, Collider portal)
     {
         currentLayer = newLayer;
-        cam.cullingMask = cam.cullingMask & (~1 << layerToHide);
+        Debug.Log("Hiding layer " + layerToHide);
+        cam.cullingMask |=  0 << layerToHide;
     }
     private void ShowLayer(int layerToShow, Collider portal)
     {
         newLayer = layerToShow;  
-        cam.cullingMask =  cam.cullingMask | 1 << layerToShow;
+        cam.cullingMask |= 1 << layerToShow;
     }
     private void ReplaceLayer(int layerToShow, int layerToHide, Collider portal)
     {
         currentLayer = layerToShow;
-        cam.cullingMask = cam.cullingMask & (~(1 << layerToHide | 1 << layerToShow));
+        cam.cullingMask &= (~(1 << layerToHide | 1 << layerToShow));
     }
 }
