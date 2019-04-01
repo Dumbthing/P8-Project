108c76abbe29c4507072806834f702ae7ff32aa5
Jannik15 <jneerd15@student.aau.dk>
2019-04-01 14:56:28 +0200

    Stencil Shader prefabs + Script support for prefabs
    Mainly in Main scene

--- P8-Project/Assets/Scripts/CameraEnabler.cs
+++ P8-Project/Assets/Scripts/CameraEnabler.cs
@@ -16,17 +16,19 @@ public class CameraEnabler : MonoBehaviour
         nextLayerCam.enabled = true;
         currentLayerCam = GetComponentInChildren<Camera>(); // Main camera
         currentLayerCam.enabled = true;
+        SetStencilShader(0);
     }
 
     public void SetNewCullingMasks(int currentRoom)
     {
-        // Reset all culling masks
+        SetStencilShader(currentRoom);
+        /// Reset all culling masks
         previousLayerCam.cullingMask = 0;
         currentLayerCam.cullingMask = 0;
         nextLayerCam.cullingMask = 0;
 
         currentLayerCam.cullingMask |= 1 << 0;    // Always keep default
-        // Bit-shift new culling masks based on layer of current room (depending on camera), without going out of bounds.
+        /// Bit-shift new culling masks based on layer of current room (depending on camera), without going out of bounds.
         currentLayerCam.cullingMask |= 1 << layout.layoutList[currentRoom].layer;
         if (currentRoom > 0)
             previousLayerCam.cullingMask |= 1 << layout.layoutList[currentRoom - 1].layer;
@@ -34,6 +36,41 @@ public class CameraEnabler : MonoBehaviour
             nextLayerCam.cullingMask |= 1 << layout.layoutList[currentRoom + 1].layer;
     }
 
+    public void SetStencilShader(int currentRoom)
+    {
+        /// Set materials in next and previous rooms to be visible through stencil mask only
+        GameObject[] goArray = FindObjectsOfType(typeof(GameObject)) as GameObject[];
+        for (int i = 0; i < goArray.Length; i++)
+        {
+            if (currentRoom > 0 )
+            {
+                if (goArray[i].layer == layout.layoutList[currentRoom - 1].layer) // Previous (Portal1)
+                {
+                    foreach (Renderer r in goArray[i].GetComponentsInChildren<Renderer>())
+                    {
+                        if (r.gameObject.tag != "StencilPrevious")
+                            r.material.shader = Shader.Find("Stencils/Portal_1/Diffuse-Equal");
+                    }
+                }
+            }
+            if (goArray[i].layer == layout.layoutList[currentRoom].layer) // Current (Portal2)
+            {
+                foreach (Renderer r in goArray[i].GetComponentsInChildren<Renderer>())
+                {
+                    if (r.material.shader != Shader.Find("Stencils/Masks/StencilMask_2"))
+                        r.material.shader = Shader.Find("Stencils/Portal_2/Diffuse-Always");
+                }
+            }
+            else if (goArray[i].layer == layout.layoutList[currentRoom + 1].layer && currentRoom < layout.layoutList.Count) // Next (Portal3)
+            {
+                foreach (Renderer r in goArray[i].GetComponentsInChildren<Renderer>())
+                {
+                    if (r.gameObject.tag != "StencilNext")
+                        r.material.shader = Shader.Find("Stencils/Portal_3/Diffuse-Equal");
+                }
+            }
+        }
+    }
     public void SetPreviousLayer(int onOrOff, int newPreviousLayer)
     {
         previousLayerCam.cullingMask |= onOrOff << newPreviousLayer;
