f2578d0e7ecd649c5d379866dc7523776128253d
Dumbthing <47414969+Dumbthing@users.noreply.github.com>
2019-04-23 13:09:54 +0200

    Scripts for doors and fixes
    Door should be drag-and-drop, assign portal to unlock in inspector.

---
+++ P8-Project/Assets/Scripts/UnlockScifiPortal.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockScifiPortal : MonoBehaviour
+{
+    Collider portalCol;
+    public GameObject portalToUnlock;
+
+    public float  doorThreshold = 0.8f;
+    float startPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        portalCol = portalToUnlock.GetComponent<BoxCollider>();
+        startPosition = transform.position.y;
+
+        if(portalCol.enabled) {
+		    portalCol.enabled = false;
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!portalCol.enabled && transform.position.y - startPosition >= doorThreshold) {
+            portalCol.enabled = true;
+        }
+        else if(portalCol.enabled && transform.position.y - startPosition < doorThreshold) {
+            portalCol.enabled = false;
+        }
+    }
+}
