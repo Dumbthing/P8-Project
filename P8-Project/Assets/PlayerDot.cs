9822567080b3b5079148d5b5adeea0f68025cc32
Guflix - Thomas Boelt <thomas@boelthansen.dk>
2019-04-30 15:40:27 +0200

    Mini Map implementation

---
+++ P8-Project/Assets/PlayerDot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDot : MonoBehaviour
+{
+    public GameObject playerObject;
+    public GameObject map;
+    private bool mapActive = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = playerObject.transform.position;
+
+        if (Input.GetKeyDown(KeyCode.M) && mapActive == false)
+        {
+            map.SetActive(true);
+            mapActive = true;
+        }
+
+        else if (Input.GetKeyDown(KeyCode.M) && mapActive == true)
+        {
+            map.SetActive(false);
+            mapActive = false;
+        }
+    }
+}
